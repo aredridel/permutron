0e8189722b74a92a6e32ba4879cc858e6f138a42
Aria Stewart <aredridel@dinhe.net>
2014-08-28 16:29:18 -0400

    Add implementation

---
+++ index.js
@@ -0,0 +1,44 @@
+var assert = require('assert');
+
+module.exports = function permutron() {
+    var args = Array.prototype.slice.apply(arguments);
+    var donecb = args.pop();
+    var iterator = args.pop();
+
+    assert(typeof iterator == 'function');
+    assert(typeof donecb == 'function');
+
+    permute(args, function(pass, next) {
+        iterator.apply(null, pass.concat(next));
+    }, donecb);
+
+};
+
+function permute(args, iterator, donecb) {
+    asyncEach(Array.isArray(args[0]) ? args[0] : [args[0]], function (cur, j, next) {
+        if (args.length == 1) {
+            iterator([cur], next);
+        } else {
+            permute(args.slice(1), function (further, next) {
+                iterator([cur].concat(further), next);
+            }, next);
+        }
+    }, donecb);
+}
+
+function asyncEach(array, iterator, done) {
+    var i = 0;
+    function iterate() {
+        if (i >= array.length) {
+            return done();
+        }
+
+        iterator(array[i], i++, iterate);
+    }
+
+    iterate();
+}
+
+function zero() {
+    return 0;
+}
